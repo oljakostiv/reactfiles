9f59bc2490a900b762786d12e8944906d9258ca7
oljakostiv <oljakostivv@gmail.com>
2021-06-29 19:13:13 +0300

    new work.

--- src/components/posts/post/Post.js
+++ src/components/posts/post/Post.js
@@ -2,18 +2,24 @@ import {getPostComments} from "../../servises/Api";
 import {useState} from "react";
 import PostComments from "./PostComments";
 
-export default function Post({item2}){
+export default function Post({item2}) {
     let [comments, setComments] = useState([]);
+    let [flag, setFlag] = useState(false)
+
 
     return (
         <div>
             <div>
-                {item2.id} - Title: {item2.title} -    <button onClick={(id) =>{
+                {item2.id} - Title: {item2.title} - <button onClick={(id) => {
                 getPostComments(item2.id).then(value => setComments(value.data))
+
+                setFlag(!flag);
+
+
             }}>Comments</button>
             </div>
 
-            <PostComments commentsOfPosts={comments}/>
+            {flag && <PostComments commentsOfPosts={comments}/>}
             <div>
                 {/*Body: {item2.body}.*/}
             </div>
